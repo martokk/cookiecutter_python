a3e023d99ed382a7f6900c5ee1dea287fc113b88
martokk <25113632+martokk@users.noreply.github.com>
2022-12-29 12:07:24 -0600

    refactoring env variable BASE_DOMAIN to BASE_URL

--- {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/_templates/fastapi/constants.py
+++ {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/_templates/fastapi/constants.py
@@ -4,4 +4,5 @@ from {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}
 
 PACKAGE_NAME = "{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}"
 PACKAGE_DESCRIPTION = "A python project created by Martokk."
-BASE_DOMAIN = str(os.environ.get("BASE_DOMAIN", f"http://{SERVER_IP}:{SERVER_PORT}"))
+BASE_DOMAIN = str(os.environ.get("BASE_DOMAIN", f"{SERVER_IP}:{SERVER_PORT}"))
+BASE_URL = str(os.environ.get("BASE_URL", f"http://{BASE_DOMAIN}"))
